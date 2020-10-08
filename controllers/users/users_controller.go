c0e976d773bc9be293bf86a4ea8249fdd60178fa
Erdenebayar Batmunkh <erdenebayar@optimal.mn>
2020-10-08 16:35:20 +0800

    fixed: user data

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,57 +1,47 @@
 package users
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/erdenebayarb/bookstore_users-api/domain/users"
 	"github.com/erdenebayarb/bookstore_users-api/services"
+	"github.com/erdenebayarb/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	fmt.Println(user)
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	fmt.Println(string(bytes))
-	if err != nil {
-		//TODO: Handle error
-		return
-	}
-
-	if err := json.Unmarshal([]byte(bytes), &user); err != nil {
-		switch err := err.(type) {
-		case *json.SyntaxError:
-			fmt.Printf("syntax error at offset %d: %v\n", err.Offset, err)
-		case *json.UnmarshalTypeError:
-			fmt.Printf("type error at offset %d: %v\n", err.Offset, err)
-		case nil:
-			// no error
-		default:
-			fmt.Printf("error %v\n", err)
-		}
-		//TODO: Handle json error
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
 		//TODO: Handle user creation error
 		return
 	}
 
-	fmt.Println(string(bytes), "user dta")
-	fmt.Println(err)
 	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
 
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
-
-// func SearchUser(c *gin.Context) {
-// 	c.String(http.StatusNotImplemented, "implement me!")
-// }
